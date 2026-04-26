d825321ce0ed422e61f64cd9ed66dcc4f02e21a5
Md Mosfequr <mosfequr11@users.noreply.github.com>
2026-04-26 23:05:04 +0600

    solve: 3 MCQ

---
+++ mcq-solve/mcq-school.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+  // 1
+  // for (i = 3; i <= 15; i = i + 2)
+  // {
+  //   printf("ICT: %d \n", i);
+  // }
+
+  // 8
+  // int i;
+  // int s = 0;
+  // for (i = 1; i <= 6; i = i + 1)
+  // {
+  //   s = s + i;
+  // }
+  // printf("%d \n", s);
+
+  int x = 100;
+  x = x / 20;
+  printf("%d", (int)pow(x, 2));
+
+  // return 0;
+}
